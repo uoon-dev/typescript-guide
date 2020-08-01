071660fb353c7364469bfd42feb9befbd7688348
mumrik <uoon.dev@gmail.com>
2020-08-02 01:24:32 +0900

    Sorter design patterns with abstract class and inheritance

---
+++ sort/src/index.ts
@@ -0,0 +1,25 @@
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+// const sorter = new Sorter(linkedList);
+linkedList.sort();
+linkedList.print();
