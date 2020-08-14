297dd643e92336419c63032b7a1799a01ffc166d
uoon-dev <uoon.dev@gmail.com>
2020-08-14 15:04:28 +0900

    ver 3.0 with Collection

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,5 +1,5 @@
-import { User, UserProps } from '../models/User';
 import { View } from './View';
+import { User, UserProps } from '../models/User';
 
 export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
