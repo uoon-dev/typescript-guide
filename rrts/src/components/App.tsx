35b0fb6b316a898980595aed03f3b07154f1f716
uoon-dev <uoon.dev@gmail.com>
2020-08-14 18:39:27 +0900

    react with typescript

---
+++ rrts/src/components/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
+import { StoreState } from '../reducers';
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
+}
+
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
